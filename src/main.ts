e6bb8b8a58559064a1148b53cac9b97d3fcbb79b
ramirachdi <rachdirami11@gmail.com>
2024-11-08 17:09:07 +0100

    changed port and enabled cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: 'http://localhost:3000',  // Allow only this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,                // Enable credentials if needed
+  });
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
